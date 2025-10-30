7f8e3f886b953fb0548cc2dfeae64e1b5cc0886e
David Huanca Ledezma <davidhuancaledezma@gmail.com>
2025-10-30 15:27:19 -0400

    feat: add routes

--- src/config/server.routes.ts
+++ src/config/server.routes.ts
@@ -1,6 +1,17 @@
 import { Router } from "express";
+import VoteRouter from "../modules/vote/votos.routes";
 
 const router = Router();
 
+router.use('/voto', VoteRouter);
+
+
+router.use((req, res) => {
+  res.status(404).send({
+    message: 'page not found ...',
+    status: 404,
+    ok: false,
+  })
+})
 
 export default router;
